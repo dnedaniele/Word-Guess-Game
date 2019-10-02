58da878ec6dea3609beb7d7d9e0006dffc2aed20
Daniele Esposito <esp.dne@gmail.com>
2019-10-02 11:38:43 +0200

    small changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 var songsOptions = ["YellowSubmarine", "EleanorRugby", "AllYouNeedIsLove", "StrawberryFieldsForever"];
 var pickSong = [];
-var showProgress;
+var showProgress= [];
 var wins = 0;
 var loss = 0;
 var guessesLeft = 15;
@@ -12,29 +12,21 @@ var userguess;
 
 // Pick Random Song
 
-document.onkeyup = function () {
+document.onkeyup = function (event) {
     pickSong = songsOptions[Math.floor(Math.random() * songsOptions.length)];
     console.log(pickSong);
     // Write on HTML "current song to be guessed:" "_ _ _ _ _ "  - pick number of letters from pickSong and reproduce the spaces
     for (var i = 0; i < pickSong.length; i++) {
         pickSong[i] = "_";
+
+        var showProgress = pickSong.length;
+
+        empty = showProgress.split("").join(" ");
+        console.log(showProgress);
+        
     }
 
-    var showProgress = pickSong.length;
-
-    showProgress = pickSong.join(" ");
-    console.log = showProgress
-   // document.getElementById("answer").innerHTML = showProgress
-}
-
-// the player guesses a letter
- 
-// prompt () {}
-// document.onkeyup = function () {
-//     var userguess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
-//     console.log(userguess); }
-
-// if the guess of the letter is right: 
+    // if the guess of the letter is right: 
 //    1. add a letter in the spaces "_ a _ _"
 //    2. ++wins
 if (userguess === pickSong) {
@@ -59,4 +51,17 @@ var html = '<h3>Press any key to start the game ;)</h3>' + '<p >Wins: ' + wins +
     '<p>Number of Guesses Remaining: ' + guessesLeft + '</p>'
 
 document.querySelector('#game').innerHTML = html;
+
+    
+   // document.getElementById("answer").innerHTML = showProgress
+}
+
+// the player guesses a letter
+ 
+// prompt () {}
+// document.onkeyup = function (event) {
+//     var userguess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
+//     console.log(userguess); }
+
+
 
